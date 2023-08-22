b298c8dd5cbce1f9fbfe0ea51b1adaacce126c48
Vitalii Melnik <mvitalim@gmail.com>
2023-08-23 01:58:58 +0300

    add link post back to blog

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getPostById } from '@/services/getPosts';
 import { Metadata } from 'next';
 import { FC } from 'react';
+import styles from './page.module.css';
+import Link from 'next/link';
 
 type Props = {
   params: {
@@ -19,12 +21,15 @@ export const generateMetadata = async ({ params: { id } }: Props): Promise<Metad
 const BlogPost: FC<Props> = async ({ params: { id } }) => {
   const post = await getPostById(id);
 
-  console.log(post);
-
   return (
     <>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
+      <div className={styles.backToBlog}>
+        <Link href='/blog' className={styles.backToBlogLink}>
+          ← Back to Blog
+        </Link>
+      </div>
     </>
   );
 };
