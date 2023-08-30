4cd2189094b83f01d7c9b777cba6ca6ed88c8f76
Vitalii Melnik <mvitalim@gmail.com>
2023-08-31 00:35:01 +0300

    add signup and mongodb

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,8 @@
+import ForgotPaSwordForm from '@/components/Form/ForgotPaswordForm';
+import { FC } from 'react';
+
+const ForgotPaSword: FC = () => {
+  return <ForgotPaSwordForm />;
+};
+
+export default ForgotPaSword;
